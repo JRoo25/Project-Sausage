bf8de70ec12e77dbf3a77e8d3700e801f06e1d12
Geoff Kevin G. Apologista <gkapologista0800@gmail.com>
2024-05-05 12:09:20 +0800

    Starting Checkpoint
    Starting Checkpoint

--- Project Sausage/Assets/Scripts/new/Checkpoint.cs
+++ Project Sausage/Assets/Scripts/new/Checkpoint.cs
@@ -1,18 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    public Vector3 checkpointPosition;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            PlayerLife playerLife = other.GetComponent<PlayerLife>();
-            if (playerLife != null)
-            {
-                playerLife.SetCheckpointPosition(transform.position);
-            }
-        }
-    }
 }
