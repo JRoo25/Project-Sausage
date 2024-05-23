98a970981c97c10254a8a192638ef41c7b082f43
Geoff Kevin G. Apologista <gkapologista0800@gmail.com>
2024-05-23 20:42:08 +0800

    Added Character Selection
    Added Character Selection

---
+++ Project Sausage/Assets/Scripts/new/Character Selection/LoadCharacter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadCharacter : MonoBehaviour {
+    public GameObject[] characterPrefabs;
+    public Transform spawnPoint;
+    public TMP_Text label;
+
+    void Start () {
+        int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        GameObject prefab = characterPrefabs[selectedCharacter];
+        GameObject clone = Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+    }
+}
